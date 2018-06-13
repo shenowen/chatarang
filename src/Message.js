54885accfd6cccae5726c142b9f0e0c5c2322215
Owen Shen <shenowen36@gmail.com>
2018-06-13 14:40:07 -0400

    Style Message/Meta

--- src/Message.js
+++ src/Message.js
@@ -1,28 +1,33 @@
 import React from 'react'
-import Avatar from './Avatar';
 
-const Message = (props) =>{
-    return(
-        <div className="Message" style={{...styles.MessageClass, ...styles.MessageDetailsClass}}>
-             
-            {props.message.userName}: {props.message.body}
+import Avatar from './Avatar'
+import Metadata from './Metadata'
+
+const Message = ({ message }) => {
+  return (
+    <div className="Message" style={styles.message}>
+      <Avatar user={message.user} />
+      <div className="details" style={styles.details}>
+        <Metadata message={message} />
+        <div className="body">
+          {message.body}
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
-var styles = {
+const styles = {
+  message: {
+    display: 'flex',
+    marginTop: '1rem',
+    padding: '0 1rem',
+  },
 
-    //.Message
-        MessageClass: {
-                display: "flex",
-                marginTop: "1rem",
-                padding: "0 1rem"
-        },
-    //.Message .details
-        MessageDetailsClass: {
-                flex: "1",
-                paddingLeft: "0.5rem"
-        }
-    }
+  details: {
+    flex: 1,
+    paddingLeft: '0.5rem',
+  }
+}
 
 export default Message
