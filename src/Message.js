06a94d03d85baad23c12c9d57f347f68e1fb05bf
Owen Shen <shenowen36@gmail.com>
2018-06-13 13:11:38 -0400

    Added style to Message and MessageForm, still wacky format

--- src/Message.js
+++ src/Message.js
@@ -2,10 +2,25 @@ import React from 'react'
 
 const Message = (props) =>{
     return(
-        <div className="Message">
+        <div className="Message" style={{...styles.MessageClass, ...styles.MessageDetailsClass}}>
             {props.message.userName}: {props.message.body}
         </div>
     )
 }
+
+var styles = {
+
+    //.Message
+        MessageClass: {
+                display: "flex",
+                marginTop: "1rem",
+                padding: "0 1rem"
+        },
+    //.Message .details
+        MessageDetailsClass: {
+                flex: "1",
+                paddingLeft: "0.5rem"
+        }
+    }
 
 export default Message
--- src/MessageForm.js
+++ src/MessageForm.js
@@ -20,6 +20,7 @@ class MessageForm extends Component{
             <form 
                 className="MessageForm"
                 onSubmit={this.handleSubmit}
+                style={styles.MessageFormClass}
             >
             <input 
                 type="text" 
@@ -27,10 +28,62 @@ class MessageForm extends Component{
                 placeholder="Type a message!"
                 value= {this.state.body}
                 onChange={this.handleChange}
+                style={{...styles.MessageForminputClass, ...styles.MessageForminputFocusClass}}
             />
-            <button type="submit">Send</button>
+            <button 
+              type="submit"
+              style={{...styles.MessageFormbuttonClass, ...styles.MessageFormChatIconClass}}
+            >
+                Send
+            </button>
             </form>
         )
+    }
+}
+
+var styles = {
+
+//.MessageForm
+    MessageFormClass: {
+            backgroundColor: "white",
+            height: "3rem",
+            display: "flex",
+            alignItems: "stretch",
+            border: "2px solid #999",
+            borderRadius: "0.5rem",
+            margin: "0.25rem",
+            padding: "0"
+    },
+//.MessageForm .chatIcon
+    MessageFormChatIconClass: {
+            display: "flex",
+            borderRadius: "0.5rem",
+            alignItems: "center",
+            backgroundColor: "white",
+            color: "#ccc",
+            padding: "0 0.5rem",
+            fontSize: "1.2rem"
+    },
+//.MessageForm input
+    MessageForminputClass: {
+            flex: "1",
+            fontSize: "1.2rem",
+            border: "0"
+    },
+//.MessageForm input:focus
+    MessageForminputFocusClass: {
+            outline: "0"
+    },
+//.MessageForm button
+    MessageFormbuttonClass: {
+            fontSize: "1.5rem",
+            backgroundColor: "#1A8FE3",
+            color: "white",
+            paddingLeft: "1rem",
+            paddingRight: "1rem",
+            borderTopRightRadius: "0.5rem",
+            borderBottomRightRadius: "0.5rem",
+            border: "1px solid white"
     }
 }
 
