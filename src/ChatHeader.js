1d80509e8948e1ee6154305881a1c40a67aa2ae4
Owen Shen <shenowen36@gmail.com>
2018-06-13 14:31:50 -0400

    Update messagelist and chat look, form at the bottom now

--- src/ChatHeader.js
+++ src/ChatHeader.js
@@ -1,44 +1,36 @@
 import React from 'react'
 
-const ChatHeader = () =>{
-    return(
-        <div 
-          className="chatHeader"
-          style={styles.ChatHeaderClass} 
-        >
-            <div 
-              className="roomInfo"
-              style={{...styles.ChatHeaderRoomInfopClass, ...styles.ChatHeaderRoomInfoh2Class}}
-            >
-                <h2>#general</h2>
-                <p>Announcements and general chat</p>
-            </div>
-        </div>
-    )
+const ChatHeader = () => {
+  return (
+    <div className="ChatHeader" style={styles.header}>
+      <div className="roomInfo">
+        <h2 style={styles.h2}>#general</h2>
+        <p style={styles.p}>Announcements and general chat</p>
+      </div>
+    </div>
+  )
 }
 
-var styles = {
+const styles = {
+  header: {
+    backgroundColor: '#f3f3f3',
+    borderBottom: '1px solid #ccc',
+    height: '3rem',
+    padding: '0 1rem',
+    display: 'flex',
+    alignItems: 'center',
+  },
 
-//.ChatHeader
-    ChatHeaderClass: {
-            backgroundColor: "#f3f3f3",
-            borderBottom: "1px solid #ccc",
-            height: "3rem",
-            padding: "0 1rem",
-            display: "flex",
-            alignItems: "center"
-    },
-//.ChatHeader .roomInfo h2
-    ChatHeaderRoomInfoh2Class: {
-            fontSize: "1.1rem",
-            margin: "0"
-    },
-//.ChatHeader .roomInfo p
-    ChatHeaderRoomInfopClass: {
-            color: "#999",
-            margin: "0",
-            fontSize: "0.8rem"
-    }
+  h2: {
+    fontSize: '1.1rem',
+    margin: 0,
+  },
+
+  p: {
+    color: '#999',
+    margin: 0,
+    fontSize: '0.8rem',
+  },
 }
 
 export default ChatHeader
