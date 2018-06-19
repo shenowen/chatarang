8652459edbd114077b757e0cdc185ec304ab0a3d
Owen Shen <shenowen36@gmail.com>
2018-06-19 14:19:04 -0400

    Timestamps added with Moment

--- src/Main.js
+++ src/Main.js
@@ -1,29 +1,51 @@
 import React, { Component } from 'react'
 
+import base from './base'
 import Sidebar from './Sidebar'
 import Chat from './Chat'
 
 class Main extends Component {
   state = {
     room: {},
+    rooms: {},
   }
 
   componentDidMount() {
-    this.loadRoom({
-      name: this.props.match.params.roomName,
-    })
+    const { roomName } = this.props.match.params
+
+    base.syncState(
+      'rooms',
+      {
+        context: this,
+        state: 'rooms',
+        then: () => this.loadRoom(roomName),
+      }
+    )
+
+
   }
 
   componentDidUpdate(prevProps) {
     if (prevProps.match.params.roomName !== this.props.match.params.roomName) {
-      this.loadRoom({
-        name: this.props.match.params.roomName,
-      })
+      this.loadRoom(this.props.match.params.roomName)
     }
   }
 
-  loadRoom = (room) => {
-    this.setState({ room })
+  loadRoom = (roomName) => {
+    if (roomName === 'new') return null
+
+    const room = this.state.rooms[roomName]
+
+    if (room) {
+      this.setState({ room })
+    } else {
+      this.loadValidRoom()
+    }
+  }
+
+  loadValidRoom = () => {
+    const roomName = Object.keys(this.state.rooms)[0]
+    this.props.history.push(`/rooms/${roomName}`)
   }
 
   render() {
@@ -32,7 +54,6 @@ class Main extends Component {
         <Sidebar
           user={this.props.user}
           signOut={this.props.signOut}
-          loadRoom={this.loadRoom}
         />
         <Chat
           user={this.props.user}
